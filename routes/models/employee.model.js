1ea6b91f74b65d9336f43d0a42773c8ee8fd68a5
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-27 18:37:03 +0100

    Use mongoose

---
+++ routes/models/employee.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: {type: String, required: true},
+    department: {type: String, required: true}
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Employee', employeeSchema);
---
+++ routes/models/product.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    client: {type: String, required: true}
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Product', productSchema);
