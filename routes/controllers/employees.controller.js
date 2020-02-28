134662142e1bfed1760017d764a123521b8da290
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-28 18:01:07 +0100

    Add missing relations to departments collection

--- routes/controllers/employees.controller.js
+++ routes/controllers/employees.controller.js
@@ -12,7 +12,7 @@ exports.getRandom = async (req, res) => {
     try {
         const count = await Employee.countDocuments();
         const rand = Math.floor(Math.random() * count);
-        const dep = await Employee.findOne().skip(rand);
+        const dep = await Employee.findOne().skip(rand).populate('department');
         if (!dep) res.status(404).json({ message: 'Not found' });
         else res.json(dep);
     } catch (err) {
@@ -22,7 +22,7 @@ exports.getRandom = async (req, res) => {
 
 exports.getById = async (req, res) => {
     try {
-        const dep = await Employee.findById(req.params.id);
+        const dep = await Employee.findById(req.params.id).populate('department');
         if (!dep) res.status(404).json({ message: 'Not found' });
         else res.json(dep);
     } catch (err) {
@@ -34,7 +34,7 @@ exports.postNew = async (req, res) => {
     try {
         const { firstName, lastName, department } = req.body;
         const newEmployee = new Employee({ firstName: firstName, lastName: lastName, department: department });
-        await newEmployee.save();
+        await newEmployee.save().then(newEmployee.populate('department').execPopulate());
         res.json(newEmployee);
     } catch (err) {
         res.status(500).json(err);
@@ -46,7 +46,7 @@ exports.modifyById = (req, res) => {
     try {
         Employee.findByIdAndUpdate(req.params.id, { $set: { firstName: firstName, lastName: lastName, department: department } }, { new: true }, (err, doc) => {
             err ? res.status(404).json({ message: 'Not found...' }) : res.json(doc);
-        });
+        }).populate('department');
     } catch (err) {
         res.status(500).json(err);
     }
@@ -56,7 +56,7 @@ exports.deleteById = (req, res) => {
     try {
         Employee.findByIdAndDelete(req.params.id, { new: false }, (err, doc) => {
             err ? res.status(404).json({ message: 'Not found...' }) : res.json(doc);
-        });
+        }).populate('department');
     } catch (err) {
         res.status(500).json(err);
     }
