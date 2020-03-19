37bff23edb751f30d47d0cba1dabb567ae30b77e
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-19 23:26:17 +0100

    Move after method within describe function to fix bug with CRUD tests

--- routes/models/test/employee.test.js
+++ routes/models/test/employee.test.js
@@ -3,44 +3,50 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 
 describe('Employee', () => {
- it('should throw error if at least one data field is missing', () => {
+    it('should throw error if at least one data field is missing', () => {
 
-    const [ firstName, lastName, department ] = ['Amanda', 'Doe', 'Marketing'];
-    const testDep1 = new Employee({});
-    const testDep2 = new Employee({ firstName: firstName, lastName: lastName });
-    const testDep3 = new Employee({ department: department });
+        const [firstName, lastName, department] = ['Amanda', 'Doe', 'Marketing'];
+        const testDep1 = new Employee({});
+        const testDep2 = new Employee({ firstName: firstName, lastName: lastName });
+        const testDep3 = new Employee({ department: department });
 
-    const cases = [testDep1, testDep2, testDep3];
-    for (let testDep of cases) {
-        testDep.validate(err => {
-            expect(err.errors).to.exist;
-        });
-    }
- });
- it('should throw error if args are not strings', () => {
-     const [ func, object, array, string ] = [function () {}, {}, [], 'loremIpsum'];
-     const testDep1 = new Employee({ firstName: string, lastName: array, department: string});
-     const testDep2 = new Employee({ firstName: object, lastName: string, department: string});
-     const testDep3 = new Employee({ firstName: string, lastName: array, department: func});
-     const testDep4 = new Employee({ firstName: object, lastName: func, department: array});
-
-     const cases = [testDep1, testDep2, testDep3, testDep4];
-
-     for (let testDep of cases) {
-         testDep.validate(err => {
-            expect(err.errors).to.exist;
-        });
-    }
- });
- it('should work properly if args are correct', () => {
-    const [ firstName, lastName, department ] = ['Amanda', 'Doe', 'Marketing'];
-    const dep = new Employee({ firstName: firstName, lastName: lastName, department: department });
-    dep.validate(err => {
-        expect(err).to.not.exist;
+        const cases = [testDep1, testDep2, testDep3];
+        for (let testDep of cases) {
+            testDep.validate(err => {
+                expect(err.errors).to.exist;
+            });
+        }
     });
- });
+    it('should throw error if args are not strings', () => {
+        const [func, object, array, string] = [function () { }, {}, [], 'loremIpsum'];
+        const testDep1 = new Employee({ firstName: string, lastName: array, department: string });
+        const testDep2 = new Employee({ firstName: object, lastName: string, department: string });
+        const testDep3 = new Employee({ firstName: string, lastName: array, department: func });
+        const testDep4 = new Employee({ firstName: object, lastName: func, department: array });
+
+        const cases = [testDep1, testDep2, testDep3, testDep4];
+
+        for (let testDep of cases) {
+            testDep.validate(err => {
+                expect(err.errors).to.exist;
+            });
+        }
+    });
+
+
+    it('should work properly if args are correct', () => {
+        const [firstName, lastName, department] = ['Amanda', 'Doe', 'Marketing'];
+        const dep = new Employee({ firstName: firstName, lastName: lastName, department: department });
+        dep.validate(err => {
+            expect(err).to.not.exist;
+        });
+    });
+    after(() => {
+        try {
+            mongoose.models = {};
+        } catch (e) {
+            console.log(e);
+        }
+    });
 });
 
-after(() => {
-    mongoose.models = {};
-});
